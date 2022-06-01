88557c8718ea7407e369eb92207b0d56f1c0558e
isfacademy <isfacademy.2023@gmail.com>
2022-06-01 13:07:54 +0300

    fix facebook login

--- lib/Controller/AuthController.dart
+++ lib/Controller/AuthController.dart
@@ -1,24 +1,24 @@
 // @dart=2.9
 
 import 'package:e_commerce_app/helper/homeviewbinding.dart';
+import 'package:e_commerce_app/helper/localStorageUserData.dart';
 import 'package:e_commerce_app/models/UserModel.dart';
 import 'package:e_commerce_app/service/FireStoreUser.dart';
-import 'package:e_commerce_app/helper/localStorageUserData.dart';
-import 'package:e_commerce_app/service/HomeViewModelService.dart';
 import 'package:e_commerce_app/views/auth/ControlView.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-import 'package:e_commerce_app/views/home_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController extends GetxController {
   bool _isloading = false;
   bool get isloading => _isloading;
   GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
   FirebaseAuth _auth = FirebaseAuth.instance;
-  static final FacebookLogin facebookSignIn = new FacebookLogin();
+  // static final FacebookLogin facebookSignIn = new FacebookLogin();
+  final fb = FacebookLogin();
+
   String email = "", password = "", name = "";
   bool _showpassword = true;
   bool get showpassword => _showpassword;
@@ -70,35 +70,78 @@ class AuthController extends GetxController {
     final AuthCredential credential = GoogleAuthProvider.credential(
         idToken: googleSignInAuthentication.idToken,
         accessToken: googleSignInAuthentication.accessToken);
-    await _auth.signInWithCredential(credential).then((user) {
-      SaveUserToFireStore(user);
+    await _auth.signInWithCredential(credential).then((usercredential) {
+      UserModel userModel = UserModel(
+          email: usercredential.user.email,
+          name: usercredential.user.displayName,
+          pic: usercredential.user.photoURL,
+          userId: usercredential.user.uid);
+
+      SaveUserToFireStore(userModel);
       _isloading = false;
       update();
     });
     Get.offAll(() => ControlView(), binding: HomeViewBinding());
-
-    // saveUserTosharedPreference(new UserModel(
-    //   googleuser.,
-    // ));
-
-    // saveUserDataTosharedPreference(UserModel(
-    //     userId: credential.token.toString(),
-    //     email: googleuser.email,
-    //     name: googleuser.displayName,
-    //     pic: googleuser.photoUrl));
-    // print("google user : ${googleuser}");
   }
 
   void facebookSignInMethod() async {
-    final FacebookLoginResult result = await facebookSignIn.logIn(['email']);
+    // final FacebookLoginResult result = await facebookSignIn.logIn(['email']);
 
-    final accessToken = result.accessToken.token;
-    if (result.status == FacebookLoginStatus.loggedIn) {
-      final facebookcredential = FacebookAuthProvider.credential(accessToken);
-      await _auth.signInWithCredential(facebookcredential).then((user) {
-        SaveUserToFireStore(user);
-      });
-      Get.offAll(() => ControlView(), binding: HomeViewBinding());
+    // final accessToken = result.accessToken.token;
+    // if (result.status == FacebookLoginStatus.loggedIn) {
+    //   final facebookcredential = FacebookAuthProvider.credential(accessToken);
+    //   await _auth.signInWithCredential(facebookcredential).then((user) {
+    //     SaveUserToFireStore(user);
+    //   });
+    //   Get.offAll(() => ControlView(), binding: HomeViewBinding());
+    // }
+
+    // Log in
+    final res = await fb.logIn(permissions: [
+      FacebookPermission.publicProfile,
+      FacebookPermission.email,
+    ]);
+
+// Check result status
+    switch (res.status) {
+      case FacebookLoginStatus.success:
+        // Logged in
+        // Send access token to server for validation and auth
+        final FacebookAccessToken accessToken = res.accessToken;
+        print('Access token: ${accessToken.token}');
+
+        // Get profile data
+        final profile = await fb.getUserProfile();
+        print('Hello, ${profile.name}! You ID: ${profile.userId}');
+
+        // Get user profile image url
+        final imageUrl = await fb.getProfileImageUrl(width: 100);
+        print('Your profile image: $imageUrl');
+
+        // Get email (since we request email permission)
+        final email = await fb.getUserEmail();
+        // But user can decline permission
+        if (email != null) print('And your email is $email');
+
+        final facebookcredential =
+            FacebookAuthProvider.credential(accessToken.token);
+        await _auth.signInWithCredential(facebookcredential).then((value) {
+          UserModel user = UserModel(
+              email: value.user.email,
+              name: profile.firstName + " " + profile.lastName,
+              pic: imageUrl,
+              userId: value.user.uid);
+          SaveUserToFireStore(user);
+        });
+
+        break;
+      case FacebookLoginStatus.cancel:
+        // User cancel log in
+        break;
+      case FacebookLoginStatus.error:
+        // Log in failed
+        print('Error while log in: ${res.error}');
+        break;
     }
   }
 
@@ -121,6 +164,7 @@ class AuthController extends GetxController {
       await _auth
           .createUserWithEmailAndPassword(email: email, password: password)
           .then((user) {
+        UserModel user = UserModel(email: email, name: name);
         SaveUserToFireStore(user);
       }); // .then((value) => print(value));
       Get.offAll(() => ControlView(), binding: HomeViewBinding());
@@ -130,14 +174,14 @@ class AuthController extends GetxController {
     }
   }
 
-  SaveUserToFireStore(UserCredential user) async {
-    UserModel userModel = UserModel(
-        userId: user.user.uid,
-        email: user.user.email,
-        name: user.user.displayName ?? name,
-        pic: user.user.photoURL ?? "");
-    await FireStoreUser().AddUserToFireStore(userModel);
-    _getCurrentUser(user.user.uid);
+  SaveUserToFireStore(UserModel user) async {
+    // UserModel userModel = UserModel(
+    //     userId: user.user.uid,
+    //     email: user.user.email,
+    //     name: user.user.displayName ?? name,
+    //     pic: user.user.photoURL ?? "");
+    await FireStoreUser().AddUserToFireStore(user);
+    _getCurrentUser(user.userId);
   }
 
   saveUserDataTosharedPreference(UserModel userModel) async {
